c989b64a3bd1c3843d5ee888904d83754f1d1481
LucaElisio <personal.elix@gmail.com>
2024-03-26 17:44:35 +0100

    feat: definition of global variables

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+// Definisco prezzo al km
+const priceKm = 0.21; //number
+
+// Definisco sconto
+let discount = 0; //number
+
+// Trovo bottone nella pagina
+const sendBtnElem = document.getElementById("send-btn"); //object | null
+
